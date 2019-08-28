137f0e2305028e621d428bb782bd8a174a8199a1
kremerme <kremerme@bu.edu>
2019-08-28 16:46:36 -0400

    more commenting

--- include/ApolloSM/svfplayer.hh
+++ include/ApolloSM/svfplayer.hh
@@ -7,19 +7,15 @@
 #include <ApolloSM/ApolloSM.hh>
 #include <stdio.h>
 
-
-
-
-
-
-
-
 class SVFPlayer : public IPBusIO {
 public:
   SVFPlayer(uhal::HwInterface * const * _hw);  
   int play(std::string const & svfFile , std::string const & XVCReg);
 private:
+
   SVFPlayer();
+
+  /* Defined in svfplayer.cc */
   int setup(std::string const & XVCReg);
   int shutdown();
   void udelay(long usecs, int tms, long num_tck);
@@ -29,6 +25,8 @@ private:
   void pulse_sck();
   void set_trst(int v);
   int set_frequency(int v);
+
+  /* defined in svfplayer_svf.cc */
   int read_command(char **buffer_p, int *len_p);
   int token2tapstate(const char *str1);
   void bitdata_free(struct bitdata_s *bd, int offset);
@@ -36,9 +34,12 @@ private:
   int getbit(unsigned char *data, int n);
   int bitdata_play(struct bitdata_s *bd, enum libxsvf_tap_state estate);
   int svf();
+
+  /* defined in svfplayer_tap.cc */
   void tap_transition(int v);
   int tap_walk(enum libxsvf_tap_state s);
-  /*internal variables */
+
+  /* internal variables */
   enum libxsvf_tap_state tap_state;
   FILE *f;
   int verbose;
@@ -48,6 +49,7 @@ private:
   int retval_i;
   int retval[256];
 
+  /* nodes for AXI connections */
   uhal::Node const * nTDI;
   uhal::Node const * nTDO;
   uhal::Node const * nTMS;

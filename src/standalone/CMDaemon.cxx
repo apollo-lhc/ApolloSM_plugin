376567d10e0ed67ac92ce9018fee22d81ffdb7e4
felex chen <felexc@bu.edu>
2019-09-25 13:22:53 -0400

    was working on seg fault

--- src/standalone/CMDaemon.cxx
+++ src/standalone/CMDaemon.cxx
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <ApolloSM/ApolloSM.hh>
 #include <uhal/uhal.hpp>
 #include <vector>
@@ -10,6 +10,7 @@
 #include <syslog.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+//#include <chrono>
 
 // ====================================================================================================
 // Definitions
@@ -23,6 +24,12 @@ struct temperatures {
   uint8_t REGTemp;
 };
 
+//typedef std::chrono::high_resolution_clock clk;
+//
+//typedef std::chrono::high_resolution_clock::time_point tp;
+//
+//typedef std::chrono::duration<double, std::milli> doubleDur;
+
 // ====================================================================================================
 // Kill program if it is in background
 bool volatile loop;
@@ -35,8 +42,8 @@ void static signal_handler(int const signum) {
 
 // ====================================================================================================
 
-temperatures sendAndParse(ApolloSM* SM) {
-  temperatures temps;
+struct temperatures sendAndParse(ApolloSM * const SM) {
+  struct temperatures temps;
   
   // read and print
   std::string recv((SM)->UART_CMD("CM.CM1", "simple_sensor", '%'));
@@ -113,10 +120,7 @@ temperatures sendAndParse(ApolloSM* SM) {
 
 // ====================================================================================================
 
-void sendTemps(ApolloSM* SM, temperatures temps) {
-
-  // TO DO use RegWriteRegister
-
+void sendTemps(ApolloSM * const SM, struct temperatures const temps) {
   (SM)->RegWriteRegister("SLAVE_I2C.S2.0", temps.MCUTemp);
   (SM)->RegWriteRegister("SLAVE_I2C.S3.0", temps.FIREFLYTemp);
   (SM)->RegWriteRegister("SLAVE_I2C.S4.0", temps.FPGATemp);
@@ -125,10 +129,18 @@ void sendTemps(ApolloSM* SM, temperatures temps) {
 
 // ====================================================================================================
 
-long elapsed(long startS, long startNS, long stopS, long stopNS) {
-  // 1000000 converts seconds to microseconds. 1000 converts nanoseconds to microseconds
-  return (stopS - startS)*1000000 + (stopNS - startNS)/1000;
-}
+  long elapsed(long const startS, long const startNS, long const stopS, long const stopNS) {
+    // 1000000 converts seconds to microseconds. 1000 converts nanoseconds to microseconds
+    printf("s: %ld\n", (stopS - startS)*1000000);
+    printf("ns: %ld\n", (stopNS - startNS)/1000);
+    return (stopS - startS)*1000000 + (stopNS - startNS)/1000;
+  }
+
+//doubleDur elapsed(tp const start) {
+//  // The difference between two time point objects is a duration object. This duration
+//  // in particular is in milliseconds so multiply by one million to get nano
+//  return ((clk::now() - start)*1000000);
+//}
 
 // ====================================================================================================
 
@@ -158,58 +170,60 @@ int main(int, char**) {
   // for counting time
   struct timespec startTS;
   struct timespec stopTS;
-
-  // second to microsecond
-  long stous = 1000000;
-  // 10 seconds in microseconds  
-  long sleepTime = 10*stous;
-
-  // ====================================================================================================
-  // For daemonizing
-  // Process and session ids
-  pid_t pid, sid;
-
-  // Fork current process
-  pid = fork();
-  // Parent process continues with a process ID greater than 0
-  if(0 < pid) {
-    exit(EXIT_SUCCESS);
-  }
-  // Process ID lower than 0 indicates failure in either process
-  else if(0 > pid) {
-    exit(EXIT_FAILURE);
-  }
-  // Parent process has now terminated and forked child process will continue
-  // PID of child process was 0
-
-  // Since the child process is a daemon, the umask needs to be set so files and logs can be written to
-  umask(0);
-  // Open system logs for child process
-  openlog("CMDaemon", LOG_NOWAIT | LOG_PID, LOG_USER);
-  syslog(LOG_NOTICE, "Successfully started CMDaemon");
-
-  // Generate a session ID for child process
-  sid = setsid();
-  // Check valid SID
-  if(0 > sid) {
-    syslog(LOG_ERR, "Could not generate session ID for child process");
-    exit(EXIT_FAILURE);
-  }
-
-  // Change current working directory to a directory guaranteed to exist
-  if(0 > chdir("/")) {
-    syslog(LOG_ERR, "Could not change working directory to /");
-    exit(EXIT_FAILURE);
-  }
-
-  // Daemons cannot use the terminal so close standard file descriptors for security reasons
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
   
-  // ====================================================================================================
+  // second to microsecond
+  // long stous = 1000000;
+  // 10 seconds in microseconds  
+  //long sleepTime = 10*stous;
 
-  temperatures temps;  
+  //  tp start;
+
+//// ====================================================================================================
+//// For daemonizing
+//// Process and session ids
+//pid_t pid, sid;
+//
+//// Fork current process
+//pid = fork();
+//// Parent process continues with a process ID greater than 0
+//if(0 < pid) {
+//  exit(EXIT_SUCCESS);
+//}
+//// Process ID lower than 0 indicates failure in either process
+//else if(0 > pid) {
+//  exit(EXIT_FAILURE);
+//}
+//// Parent process has now terminated and forked child process will continue
+//// PID of child process was 0
+//
+//// Since the child process is a daemon, the umask needs to be set so files and logs can be written to
+//umask(0);
+//// Open system logs for child process
+//openlog("CMDaemon", LOG_NOWAIT | LOG_PID, LOG_USER);
+//syslog(LOG_NOTICE, "Successfully started CMDaemon");
+//
+//// Generate a session ID for child process
+//sid = setsid();
+//// Check valid SID
+//if(0 > sid) {
+//  syslog(LOG_ERR, "Could not generate session ID for child process");
+//  exit(EXIT_FAILURE);
+//}
+//
+//// Change current working directory to a directory guaranteed to exist
+//if(0 > chdir("/")) {
+//  syslog(LOG_ERR, "Could not change working directory to /");
+//  exit(EXIT_FAILURE);
+//}
+//
+//// Daemons cannot use the terminal so close standard file descriptors for security reasons
+//close(STDIN_FILENO);
+//close(STDOUT_FILENO);
+//close(STDERR_FILENO);
+//
+//// ====================================================================================================
+
+  struct temperatures temps;  
 
   //Set the power-up done bit to 1 for the IPMC to read
   SM->RegWriteRegister("SLAVE_I2C.S1.SM.STATUS.DONE",1);
@@ -217,7 +231,10 @@ int main(int, char**) {
   while(loop) {
     // start time
     clock_gettime(CLOCK_REALTIME, &startTS);
+    //start = clk::now();
 
+    if(!loop) {break;}
+    
     if(SM->RegReadRegister("CM.CM1.CTRL.PWR_GOOD")){
       temps = sendAndParse(SM);
       sendTemps(SM, temps);
@@ -226,12 +243,22 @@ int main(int, char**) {
       sendTemps(SM, temps);
     }
 
+    //printf("Read done\n");
+    
     // end time
     clock_gettime(CLOCK_REALTIME, &stopTS);
-      
+    
+    if(!loop) {break;}
+    
+    printf("No kill\n");
+
     // sleep for 10 seconds minus how long it took to read and send temperature
-    usleep(sleepTime - elapsed(startTS.tv_sec, startTS.tv_nsec, stopTS.tv_sec, stopTS.tv_nsec));
+    //usleep(sleepTime - elapsed(startTS.tv_sec, startTS.tv_nsec, stopTS.tv_sec, stopTS.tv_nsec));
+    //usleep(sleepTime - elapsed(start).count());
+    usleep(10000000);
   }
+
+  printf("Out of loop\n");
   
   // ====================================================================================================
   
@@ -250,9 +277,9 @@ int main(int, char**) {
   
   // ====================================================================================================
   
-  syslog(LOG_NOTICE, "Successful kill");
-  closelog();
-  //  exit(EXIT_SUCCESS);
+//syslog(LOG_NOTICE, "Successful kill");
+//closelog();
+////  exit(EXIT_SUCCESS);
 
   // ====================================================================================================
   

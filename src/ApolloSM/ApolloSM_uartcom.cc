1073ab66280f8ad06eb1765041c01794c3132181
felexchen <felexc@bu.edu>
2019-10-21 16:39:41 -0400

    working on tty UART

--- src/ApolloSM/ApolloSM_uartcom.cc
+++ src/ApolloSM/ApolloSM_uartcom.cc
@@ -1,5 +1,5 @@
 #include <ApolloSM/ApolloSM.hh>
-#include <ApolloSM_Exceptions/ApolloSM_Exceptions.hh>
+#include <ApolloSM/ApolloSM_Exceptions.hh>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -153,12 +153,14 @@ void ApolloSM::writeByte() {
   
 }
 */
-std::string ApolloSM::UART_CMD(std::string i, std::string sendline, char const promptChar) {
+std::string ApolloSM::UART_CMD(std::string i, std::string, char const) {
   // 1 for CM1, 2 for CM2, 3 for ESM
   std::string file = "/dev/ttyUL" + i;
   
   // file descriptor
   int fd = open(file.c_str(), O_RDWR);
+
+  printf("The file descriptor is: %d\n", fd);
 
   // ==================================================
   // For pselect
@@ -178,37 +180,32 @@ std::string ApolloSM::UART_CMD(std::string i, std::string sendline, char const p
 
   // timeout for reading
   struct timespec t;
-  t.tv_sec = 5; // seconds
-  t.tv_nsec = 0; // nanoseconds
+  t.tv_sec = 0; // seconds
+  t.tv_nsec = 100000000; // 100 milliseconds
   // ==================================================
   
   char readChar;
-  char last = 0;
+  //  char last = 0;
 
   std::string recvline;
 
   //Press enter
-  char enter[2];
-  enter[0] = '\r';
-  enter[1] = '\n';
-  std::size_t enterLen = 2;
-  write(fd, enter, enterLen);
+  char enter = 0xd;
+  //  enter[0] = '\r';
+  //  enter[0] = '\n';
+  //std::size_t enterLen = 1;
+  //  write(fd, enter, enterLen);
+  write(fd, &enter,1);
+  write(fd, &enter,1);
   usleep(10000);
 
-  //remove any '\n's
-  size_t pos = std::string::npos;
-  while((pos = sendline.find('\n')) != std::string::npos){
-    sendline.erase(sendline.begin()+pos);
-  }
-  //remote any '\r's
-  while((pos = sendline.find('\r')) != std::string::npos){
-    sendline.erase(sendline.begin()+pos);
-  }
+  printf("First enter sent\n");
 
   // ==================================================
   //make sure the buffer is clear  
   bool readNotTimedOut = 1;
   while(readNotTimedOut) {
+    //    printf("%ld %ld\n", t.tv_sec, t.tv_nsec);
     // make copy of readSet everytime pselect is used because we don't want contents of readSet changed
     fd_set readSetCopy = readSet;
     int returnVal;
@@ -228,98 +225,125 @@ std::string ApolloSM::UART_CMD(std::string i, std::string sendline, char const p
 	e.Append("read error: error reading from " + file + "\n");
 	throw e;
       }
+      printf("%c", readChar);
+      fflush(stdout);
     }
   }
+  
+  printf("Buffer cleared\n");
 
-  // ==================================================  
-  //Write the command
-  // Write one byte and then immediately read one byte to make sure they are the same
-  for(size_t i = 0; i < sendline.size();i++){
-    //Wait for write buffer in file descriptor to be not full
-    
-    // make copy of wroteSet everytime pselect is used because we don't want contents of writeSet changed
-    fd_set writeSetCopy = writeSet;
-    int returnValw;
-    // Pselect returns 0 for time out, -1 for error, or number of file descriptors ready to be written to
-    if(0 == (returnValw = pselect(maxfdp1, NULL, &writeSetCopy, NULL, &t, NULL)))  {
-      // If the buffer is full for more than 5 seconds something is probably wrong
-      BUException::IO_ERROR e;
-      e.Append("pselect timed out writing to " + file + " in 5 seconds\n");
-      throw e;
-    } else if(-1 == returnValw) {
-      BUException::IO_ERROR e;
-      e.Append("write error: error from pselect while waiting for writer buffer in " + file + " to clear\n");
-      throw e;
-    } else {
-      // Write char
-      char writeChar[1];
-      std::size_t writeCharLen = 1;
-      writeChar[0] = sendline[i];
-      if(write(fd, writeChar, writeCharLen) < 0) {
-	BUException::IO_ERROR e;
-	e.Append("write error: error writing to " + file + "\n");
-	throw e;
-      }
-    }
-    
-    //wait for character to be echoed back
-    // make copy of readSet everytime pselect is used because we don't want contents of readSet changed
-    fd_set readSetCopy = readSet;
-    int returnValr;
-    // Pselect returns 0 for time out, -1 for error, or number of file descriptors ready to be read
-    if(0 == (returnValr = pselect(maxfdp1, &readSetCopy, NULL, NULL, &t, NULL)))  {
-      // If it takes longer than 5 seconds for a character to be echoed back something is probably wrong
-      BUException::IO_ERROR e;
-      e.Append("pselect timed out while polling " + file + " for echoed command\n");
-    } else if(-1 == returnValr) {
-      BUException::IO_ERROR e;
-      e.Append("read error: error from pselect polling " + file + " for echoed command\n");
-      throw e;
-    } else {
-      // make sure echoed character is same as sent character
-      if(read(fd, &readChar, sizeof(readChar)) < 0) {
-	BUException::IO_ERROR e;
-	e.Append("read error: error reading echoed command from " + file + "\n");
-	throw e;
-      }
-    }
- 
-    if(sendline[i] != readChar){
-      printf("Error: mismatched character %c %c\n",sendline[i],readChar);
-    }
-  }
-
-  //Press enter
-  write(fd, enter, enterLen);
-
-  // read until prompt character
-  readNotTimedOut = 1;
-  while(readNotTimedOut) {
-    fd_set readSetCopy = readSet;
-    int returnVal;
-    if(0 == (returnVal = pselect(maxfdp1, &readSetCopy, NULL, NULL, &t, NULL))) {
-      // timedout
-      readNotTimedOut = 0;
-      continue;
-    } else if (-1 == returnVal) {
-      BUException::IO_ERROR e;
-      e.Append("pselect error while polling read fd from " + file + " after command was sent and echoed\n");
-      throw e;
-    } else {
-      if(0 > read(fd, &readChar, sizeof(readChar))) {
-	BUException::IO_ERROR e;
-	e.Append("read error: error reading from " + file + " after command was sent and echoed\n");
-	throw e;
-      }
-    }
-    // Currently, we tell the difference between a regular '>' and the prompt character, also '>',
-    // by checking if the previous character was a newline
-    if((promptChar == readChar) && (('\n' == last) || ('\r' == last))) {
-      break;
-    }
-    last = readChar;
-    recvline.push_back(readChar);
-  }
-
+//////  // ==================================================  
+//////  // Remove trailing carriage and line feed from message
+//////  //remove any '\n's
+//////  size_t pos = std::string::npos;
+//////  while((pos = sendline.find('\n')) != std::string::npos){
+//////    sendline.erase(sendline.begin()+pos);
+//////  }
+//////  //remote any '\r's
+//////  while((pos = sendline.find('\r')) != std::string::npos){
+//////    sendline.erase(sendline.begin()+pos);
+//////  }
+//////
+//////  // ==================================================  
+//////  //Write the command
+//////  // Write one byte and then immediately read one byte to make sure they are the same
+//////  for(size_t i = 0; i < sendline.size();i++){
+//////    //Wait for write buffer in file descriptor to be not full
+//////    printf("%ld %ld\n", t.tv_sec, t.tv_nsec);
+//////    // make copy of wroteSet everytime pselect is used because we don't want contents of writeSet changed
+//////    fd_set writeSetCopy = writeSet;
+//////    int returnValw;
+//////    // Pselect returns 0 for time out, -1 for error, or number of file descriptors ready to be written to
+//////    if(0 == (returnValw = pselect(maxfdp1, NULL, &writeSetCopy, NULL, &t, NULL)))  {
+//////      // If the buffer is full for more than 5 seconds something is probably wrong
+//////      BUException::IO_ERROR e;
+//////      e.Append("pselect timed out writing to " + file + " in 5 seconds\n");
+//////      throw e;
+//////    } else if(-1 == returnValw) {
+//////      BUException::IO_ERROR e;
+//////      e.Append("write error: error from pselect while waiting for writer buffer in " + file + " to clear\n");
+//////      throw e;
+//////    } else {
+//////      // Write char
+//////      char writeChar[1];
+//////      std::size_t writeCharLen = 1;
+//////      writeChar[0] = sendline[i];
+//////      if(write(fd, writeChar, writeCharLen) < 0) {
+//////	BUException::IO_ERROR e;
+//////	e.Append("write error: error writing to " + file + "\n");
+//////	throw e;
+//////      }
+//////      printf("sent: %c\n", sendline[i]);
+//////      printf("sent: %c\n", writeChar[0]);
+//////    }
+//////    
+//////    printf("%ld %ld\n", t.tv_sec, t.tv_nsec);
+//////
+//////    //wait for character to be echoed back
+//////    // make copy of readSet everytime pselect is used because we don't want contents of readSet changed
+//////    fd_set readSetCopy = readSet;
+//////    int returnValr;
+//////    // Pselect returns 0 for time out, -1 for error, or number of file descriptors ready to be read
+//////    if(0 == (returnValr = pselect(maxfdp1, &readSetCopy, NULL, NULL, &t, NULL)))  {
+//////      // If it takes longer than 5 seconds for a character to be echoed back something is probably wrong
+//////      BUException::IO_ERROR e;
+//////      e.Append("pselect timed out while polling " + file + " for echoed command\n");
+//////      throw e;
+//////    } else if(-1 == returnValr) {
+//////      BUException::IO_ERROR e;
+//////      e.Append("read error: error from pselect polling " + file + " for echoed command\n");
+//////      throw e;
+//////    } else {
+//////      // make sure echoed character is same as sent character
+//////      if(read(fd, &readChar, sizeof(readChar)) < 0) {
+//////	BUException::IO_ERROR e;
+//////	e.Append("read error: error reading echoed command from " + file + "\n");
+//////	throw e;
+//////      }
+//////      printf("read: %d\n", readChar);
+//////    }
+////// 
+//////    if(sendline[i] != readChar){
+//////      printf("Error: mismatched character %c %c\n",sendline[i],readChar);
+//////      printf("Error: mismatched character %c %d\n",sendline[i],readChar);
+//////      return "Mismatched character\n";
+//////    }
+//////    
+//////  }
+//////
+//////  //Press enter
+//////  write(fd, enter, enterLen);
+//////
+//////  printf("Second enter pressed\n");
+//////
+//////  // read until prompt character
+//////  readNotTimedOut = 1;
+//////  while(readNotTimedOut) {
+//////    fd_set readSetCopy = readSet;
+//////    int returnVal;
+//////    if(0 == (returnVal = pselect(maxfdp1, &readSetCopy, NULL, NULL, &t, NULL))) {
+//////      // timedout
+//////      readNotTimedOut = 0;
+//////      continue;
+//////    } else if (-1 == returnVal) {
+//////      BUException::IO_ERROR e;
+//////      e.Append("pselect error while polling read fd from " + file + " after command was sent and echoed\n");
+//////      throw e;
+//////    } else {
+//////      if(0 > read(fd, &readChar, sizeof(readChar))) {
+//////	BUException::IO_ERROR e;
+//////	e.Append("read error: error reading from " + file + " after command was sent and echoed\n");
+//////	throw e;
+//////      }
+//////    }
+//////    // Currently, we tell the difference between a regular '>' and the prompt character, also '>',
+//////    // by checking if the previous character was a newline
+//////    if((promptChar == readChar) && (('\n' == last) || ('\r' == last))) {
+//////      break;
+//////    }
+//////    last = readChar;
+//////    recvline.push_back(readChar);
+//////  }
+//////
   return recvline;
 }

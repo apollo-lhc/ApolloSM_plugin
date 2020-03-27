2e827a0f1b438bc6d33ea5bff61dbbfcdd985755
felexchen <felexc@bu.edu>
2020-03-27 17:55:05 -0400

    factorizing code

--- src/standalone/SM_boot.cxx
+++ src/standalone/SM_boot.cxx
@@ -21,6 +21,8 @@
 
 #include <syslog.h>  ///for syslog
 
+#include <standalone/daemon.hh>
+
 #define SEC_IN_US  1000000
 #define NS_IN_US 1000
 
@@ -33,54 +35,55 @@
 
 #define DEFAULT_SENSORS_THROUGH_ZYNQ true // This means: by default, read the sensors through the zynq
 
-// ====================================================================================================
-void daemonizeMyself(std::string pidFileName, std::string runPath) {
-
-  pid_t pid, sid;
-  pid = fork();
-  if(pid < 0){
-    //Something went wrong.
-    //log something
-    exit(EXIT_FAILURE);
-  }else if(pid > 0){
-    //We are the parent and created a child with pid pid
-    FILE * pidFile = fopen(pidFileName.c_str(),"w");
-    fprintf(pidFile,"%d\n",pid);
-    fclose(pidFile);
-    exit(EXIT_SUCCESS);
-  }else{
-    // I'm the child!
-    //open syslog
-    openlog(NULL,LOG_CONS|LOG_PID,LOG_DAEMON);
-  }
- 
-  
-  //Change the file mode mask to allow read/write
-  umask(0);
- 
-  //Start logging
-  syslog(LOG_INFO,"Opened log file\n");
- 
-  // create new SID for the daemon.
-  sid = setsid();
-  if (sid < 0) {
-    syslog(LOG_ERR,"Failed to change SID\n");
-    exit(EXIT_FAILURE);
-  }
-  syslog(LOG_INFO,"Set SID to %d\n",sid);
- 
-  //Move to RUN_DIR
-  if ((chdir(runPath.c_str())) < 0) {
-    syslog(LOG_ERR,"Failed to change path to \"%s\"\n",runPath.c_str());    
-    exit(EXIT_FAILURE);
-  }
-  syslog(LOG_INFO,"Changed path to \"%s\"\n", runPath.c_str());    
- 
-  //Everything looks good, close the standard file fds.
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
-}
+// ====================================================================================================}
+//void daemonizeMyself(std::string pidFileName, std::string runPath) {
+//
+//  pid_t pid, sid;
+//  pid = fork();
+//  if(pid < 0){
+//    //Something went wrong.
+//    //log something
+//    exit(EXIT_FAILURE);
+//  }else if(pid > 0){
+//    //We are the parent and created a child with pid pid
+//    FILE * pidFile = fopen(pidFileName.c_str(),"w");
+//    fprintf(pidFile,"%d\n",pid);
+//    fclose(pidFile);
+//    exit(EXIT_SUCCESS);
+//  }else{
+//    // I'm the child!
+//    //open syslog
+//    openlog(NULL,LOG_CONS|LOG_PID,LOG_DAEMON);
+//  }
+//
+//  syslog(LOG_INFO, "Factorize successful!\n");
+//  
+//  //Change the file mode mask to allow read/write
+//  umask(0);
+// 
+//  //Start logging
+//  syslog(LOG_INFO,"Opened log file\n");
+// 
+//  // create new SID for the daemon.
+//  sid = setsid();
+//  if (sid < 0) {
+//    syslog(LOG_ERR,"Failed to change SID\n");
+//    exit(EXIT_FAILURE);
+//  }
+//  syslog(LOG_INFO,"Set SID to %d\n",sid);
+// 
+//  //Move to RUN_DIR
+//  if ((chdir(runPath.c_str())) < 0) {
+//    syslog(LOG_ERR,"Failed to change path to \"%s\"\n",runPath.c_str());    
+//    exit(EXIT_FAILURE);
+//  }
+//  syslog(LOG_INFO,"Changed path to \"%s\"\n", runPath.c_str());    
+// 
+//  //Everything looks good, close the standard file fds.
+//  close(STDIN_FILENO);
+//  close(STDOUT_FILENO);
+//  close(STDERR_FILENO);
+//}
 
 // ====================================================================================================
 // signal handling

7c3defb2091622281e139671369626e4555d61ac
felexchen <felexc@bu.edu>
2020-04-14 17:13:47 -0400

    added CM and FPGA classes

--- src/standalone/CM.cc
+++ src/standalone/CM.cc
@@ -2,6 +2,21 @@
 
 #include <string>
 #include <vector>
+#include <boost/program_options.hh>
+
+int howMany(boost::program_options::parsed_options po, std::string option) {
+  int count = 0;
+  int totalNumOptions = po.options.size();
+
+  for(int i = 0; i < totalNumOptions; i++) {
+    std::string nextOption = po.options[i].string_key;
+    if(0 == nextOption.compare(option)) {
+      count++;
+    }
+  }
+
+  return count;
+} 
 
 CM::CM() {
 
@@ -11,6 +26,11 @@ CM::~CM() {
 
 }
 
+CM::PowerUp(boost::program_ioptions::variables_map vm) {
+  powerGood    = "";
+  bool powerUp = false;
+  
+}
 
 // std::vector<FPGA> FPGAs;
 //   

fc3244a5c4f16965420290b3bec2d6c67e2c8bd3
alux <adam.brayton.lux@cern.ch>
2021-06-16 18:41:58 -0400

    test without lpmnode 2

--- src/ApolloSM_device/ApolloSM_device.cc
+++ src/ApolloSM_device/ApolloSM_device.cc
@@ -565,7 +565,8 @@ CommandReturn::status ApolloSMDevice::EyeScan(std::vector<std::string> strArg, s
 
   uint32_t maxPrescale = strtoul(strArg[5].c_str(), NULL, 0);
   printf("The max prescale is: %d\n", maxPrescale);
-  std::vector<eyescanCoords> esCoords = SM->EyeScan(baseNode, lpmNode, horzIncrement, vertIncrement, maxPrescale);
+  //std::vector<eyescanCoords> esCoords = SM->EyeScan(baseNode, lpmNode, horzIncrement, vertIncrement, maxPrescale);
+  std::vector<eyescanCoords> esCoords = SM->EyeScan(baseNode, horzIncrement, vertIncrement, maxPrescale);
 
 //  int fd = open(fileName, O_CREAT | O_RDWR, 0644);
 //

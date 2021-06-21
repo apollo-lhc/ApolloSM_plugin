102205632a626f92d112f6b626c78ee07932d104
alux <adam.brayton.lux@cern.ch>
2021-06-21 10:45:51 -0400

    python script update

--- python/eyescan.py
+++ python/eyescan.py
@@ -6,11 +6,12 @@ import numpy as np
 import sys
 import datetime
 
-if(2 != len(sys.argv)):
+if(3 != len(sys.argv)):
     print("Wrong number of arguments")
     sys.exit()
 
 file = sys.argv[1]
+title = sys.argv[2]
 
 # get data
 data = np.genfromtxt(file,delimiter=' ')
@@ -40,6 +41,7 @@ y=np.unique(y)
 X, Y = np.meshgrid(x,y,sparse=True)
 # reshape z some kind of square
 Z = z.reshape(len(y), len(x))
+z_min, z_max = -abs(z).max(), abs(z).max()
 
 print("meshgrid and reshape done")
 
@@ -50,17 +52,20 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_xlabel('phase offset (UI fraction)')
 ax.set_ylabel('voltage offset (unknown units)')
-ax.set_title(datetime.datetime.now())
+ax.set_title(title)
 
 # log norm to set color bar scaling
 # cmap to set red-blue color scheme 
-pcm = ax.pcolor(X, Y, Z, norm=mpl.colors.LogNorm(vmin=Z.min(), vmax=Z.max()), cmap='jet')
+
+#pcm = ax.pcolor(X, Y, Z, norm=mpl.colors.LogNorm(vmin=abs(Z.min()), vmax=Z.max()), cmap='jet', shading='auto')
+pcm = ax.pcolor(X,Y,Z, cmap='jet', vmin=0, vmax=z_max, shading="auto")
 # display color bar
-fig.colorbar(pcm) 
+fig.colorbar(pcm, ax=ax) 
 
 print("Saving now")
 
 # -4 because we don't want '.txt'
+
 fig.savefig(file[:-4] + '.png')
 
-print("saved")
+print("Saved")

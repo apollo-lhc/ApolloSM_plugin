1094ea7219f68cf00d2a1fec90dce4ac0f18480d
Alp Akpinar <aakpinar@bu.edu>
2022-04-21 19:58:17 -0400

    Fix for legacy UIO finder method

--- src/ApolloSM/svfplayer.cc
+++ src/ApolloSM/svfplayer.cc
@@ -159,6 +159,15 @@ int SVFPlayer::play(std::string const & svfFileName , std::string const & XVCLab
   tap_state = LIBXSVF_TAP_INIT;
 
   int nUIO = label2uio(XVCLabel);
+  // Fall back to the legacy method if we can't find anything
+  if (nUIO == -1) {
+    nUIO = label2uio_old(XVCLabel);
+  }
+
+  // If we still can't find anything, throw an exception
+  if (nUIO == -1) {
+    throw std::runtime_error("Failed to open UIO device");    
+  }
 
   size_t const uioFileNameLength = 1024;
   char * uioFileName = new char[uioFileNameLength+1];
@@ -168,9 +177,6 @@ int SVFPlayer::play(std::string const & svfFileName , std::string const & XVCLab
   //Run setup
   fdUIO = open(uioFileName,O_RDWR);
   delete [] uioFileName;
-  if (fdUIO < 0) {
-    throw std::runtime_error("Failed to open UIO device");    
-  }
   
   jtag_reg = (sXVC volatile*) mmap(NULL, sizeof(sXVC) + offset*sizeof(uint32_t),
 				    PROT_READ|PROT_WRITE, MAP_SHARED,

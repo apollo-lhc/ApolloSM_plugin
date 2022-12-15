20c023bcc4c8bb1fc1a86eb41b98770f7331ed4a
Dan Gastler <dgastler@gmail.com>
2022-12-15 17:16:57 -0500

    Some debugging to get the version capture to work properly

--- include/ApolloSM_device/Version.hh
+++ include/ApolloSM_device/Version.hh
@@ -1,8 +1,6 @@
 #ifndef __VERSION_HH__
 #define __VERSION_HH__
-//#include <BUTool/VersionTracker.hh>
-//extern VersionTracker Version;
-#include <string>
+
 #include <vector>
 #include <stdint.h>
 extern std::vector<uint32_t> const nVer;
--- mk/version.mk
+++ mk/version.mk
@@ -1,15 +1,19 @@
-GIT_VERSION="$(shell git --no-pager  show -s --format="commit date %ci  %d"  ; git describe  --dirty --always --tags )"
-GIT_HASH="$(shell git rev-parse --verify HEAD)"
-GIT_REPO="$(shell git remote -v)"
+GIT_VERSION="$(shell git describe  --dirty --always --tags  )"
+GIT_HASH=$(shell git rev-parse --verify HEAD | awk 'BEGIN{line="";comma=""}{for (i=0;i<length($$0);i+=8){line =line comma " 0x" substr($$0,i,8);comma=","}}END{print line}')
+GIT_REPO="$(shell git ls-remote --get-url)"
 
+VERSION_FILE=src/ApolloSM_device/Version.cc
 
 define VERSION_BODY
 #include <ApolloSM_device/Version.hh>                                                                                                   
-VersionTracker const Version = VersionTracker({$(GIT_HASH)},
-                                              $(GIT_VERSION),
-                                              $(GIT_REPO));
+std::vector<uint32_t> const nVer({$(GIT_HASH)});
+char const sVer[]           = $(GIT_VERSION);
+char const uri[]            = $(GIT_REPO);
 endef
 export VERSION_BODY
 
-all:
-        @echo "$$VERSION_BODY">src/ApolloSM_device/version.cc
+$(VERSION_FILE):
+	@echo "$$VERSION_BODY">$(VERSION_FILE)
+
+clean_version:
+	@rm -f $(VERSION_FILE)
